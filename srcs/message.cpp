0b4be233db9a009406ff4222cf4a649656e9241b
Franck <duzant.franck@gmail.com>
2024-05-28 20:40:53 +0200

    EmptyMSGCrashFix

--- srcs/message.cpp
+++ srcs/message.cpp
@@ -13,7 +13,7 @@ void Server::_initCmdMap()
 }
 
 template <typename T>
-void PrintVectArgs(std::vector<T> args)
+static void PrintVectArgs(std::vector<T> args)
 {
 	for (long unsigned int i = 0; i < args.size(); i++)
 		std::cout << "Arg " << i << ": " << args[i] << std::endl;
@@ -21,9 +21,15 @@ void PrintVectArgs(std::vector<T> args)
 
 void Server::parseInput(std::string str)
 {
+	if (str.empty())
+	{
+		if (DEBUG == 1)
+			std::cerr << "Received empty string" << std::endl;
+		return ;
+	}
 	std::cout << "Received: " << str << std::endl;
 	str.erase(std::remove(str.begin(), str.end(), '\n'), str.end());
-	
+
 	std::string s;
 	std::stringstream ss(str);
 	std::vector<std::string> args;
@@ -37,4 +43,3 @@ void Server::parseInput(std::string str)
 	if (_cmdmap[args[0]] != NULL)
 		_cmdmap[args[0]]();
 }
-
